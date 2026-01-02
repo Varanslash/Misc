235c3f63db9675641b86c247055e7707c6e23962
Varanslash <odiaenvy@gmail.com>
2026-01-02 15:40:13 -0800

    Update tool.ps1

--- tool.ps1
+++ tool.ps1
@@ -1,10 +1,19 @@
 $counter = 0 # variable
 Add-Type -AssemblyName PresentationFramework # load assembly for message box
-Add-Type -AssemblyName System.Media # load assembly for sound
 do {
     [System.Windows.MessageBox]::Show("you are an idiot") # lol
-    [System.Media.SystemSounds]::Exclamation.Play() # play sound
     New-Item -Path "C:\Users\$env:USERNAME\Downloads\YOUR COMPUTER HAS BEEN INFECTED $counter.txt" -ItemType File # spawns new file to scare recipient
     Set-Content -Path "C:\Users\$env:USERNAME\Downloads\YOUR COMPUTER HAS BEEN INFECTED $counter.txt" -Value "you are an idiot" # lol
     $counter++ # increment counter
+
 } while ($counter -lt 20) # repeat 20 times
+
+<#
+Essentially what this virus does is show a message box with a message reminiscent of "you are an idiot" from a while ago, 
+then spawns a .txt file with the title "YOUR COMPUTER HAS BEEN INFECTED" and text saying "you are an idiot". That's about it.
+
+If you want further explanation on the script, ask a knowledgeable friend or paste it into ChatGPT, and if you don't feel safe running it, I won't mind! Just don't
+run it and move on with your day.
+
+Have fun nerds!
+#>
